644d99a1d51eca6fb5644ebac34bc940bc643c0e
“Fernanda <maryfermtzlb@gmail.com>
2024-09-18 19:08:33 -0600

    fix(Modal and Hero): Change style and img

--- src/componentes/Hero.jsx
+++ src/componentes/Hero.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import ModalForm from "./ModalForm";
 import video1 from "../assets/video1.mp4";
 import video2 from "../assets/video2.mp4";
+import logo from "../assets/logo.jpg";
 import {
   FaFacebookF,
   FaInstagram,
@@ -17,6 +18,7 @@ const Hero = () => {
   return (
     <div className="flex flex-col items-center mt-28 mb-28 lg:mt-20">
       <ModalForm isOpen={modalInfoForm.isOpen} onClose={closeModalForm} />
+      <img className="h-40 w-40 my-5 animate-pulse" src={logo} alt="Logo" />
       <h1 className="text-4xl sm:text-6xl lg:text-7xl text-center tracking-wide">
         Grupo Musical <br></br>
         <span className="bg-gradient-to-r from-yellow-500 to-yellow-800 text-transparent bg-clip-text">
