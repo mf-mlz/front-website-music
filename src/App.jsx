60ffae34ff4f859073673ca7aeb31f0792e27223
“Fernanda <maryfermtzlb@gmail.com>
2024-09-18 19:13:44 -0600

    fix(Modal and Hero): Change style and img

--- src/App.jsx
+++ src/App.jsx
@@ -66,7 +66,7 @@ const App = () => {
   return (
     <>
       <Navbar />
-      <div className="max-w-[90%] mx-auto pt-10 px-6">
+      <div className="max-w-[90%] mx-auto px-6">
         <div id="Hero">
           <Hero />
         </div>
